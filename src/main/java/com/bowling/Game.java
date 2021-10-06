acc03cfd2db81fcd3cc5547850bd36b599ec24ce
Harm De Weirdt <harm.deweirdt@mailfence.com>
2021-10-06 19:03:02 +0200

    behaviour: a new game starts with a score of 0

--- src/main/java/com/bowling/Game.java
+++ src/main/java/com/bowling/Game.java
@@ -1,7 +1,7 @@
 package com.bowling;
 
 public class Game {
-    public static void main(String[] args) {
-        System.out.println("Bowling!");
+    public int score() {
+        return 0;
     }
 }
--- src/test/groovy/com/bowling/GameTest.groovy
+++ src/test/groovy/com/bowling/GameTest.groovy
@@ -3,4 +3,8 @@ package com.bowling
 import spock.lang.Specification
 
 class GameTest extends Specification {
+    def "A game starts with 0 points" () {
+        expect:
+        new Game().score() == 0
+    }
 }
