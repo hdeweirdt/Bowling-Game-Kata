8ec4b51e178529f1b6684926e487897689918d9e
Harm De Weirdt <harm.deweirdt@mailfence.com>
2021-10-06 19:07:50 +0200

    behaviour: rolls knocked down are added to the score

--- src/main/java/com/bowling/Game.java
+++ src/main/java/com/bowling/Game.java
@@ -1,7 +1,13 @@
 package com.bowling;
 
 public class Game {
+    private int score;
+
+    public void roll(int numberOfPinsKnockedDown) {
+       score += numberOfPinsKnockedDown;
+    }
+
     public int score() {
-        return 0;
+        return score;
     }
 }
--- src/test/groovy/com/bowling/GameTest.groovy
+++ src/test/groovy/com/bowling/GameTest.groovy
@@ -3,8 +3,19 @@ package com.bowling
 import spock.lang.Specification
 
 class GameTest extends Specification {
-    def "A game starts with 0 points" () {
+    def "A game starts with 0 points"() {
         expect:
         new Game().score() == 0
+    }
+
+    def "When a a player rolls, the number of pins knocked down is added to the score"() {
+        given: "a newly started game"
+        Game game = new Game()
+
+        when: "the player knocks pins down"
+        game.roll(5)
+
+        then: "the score is updated with the number of pins knocked down"
+        game.score() == 5
     }
 }
